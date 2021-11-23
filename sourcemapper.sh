f31ac42f1b8350dbfa50b2c7c8c44dd8dd48609e
MCApollo <34170230+MCApollo@users.noreply.github.com>
2021-11-22 19:38:48 -0600

    Misc

--- sourcemapper.sh
+++ sourcemapper.sh
@@ -3,7 +3,9 @@
 JOBLIMIT=${JOBLIMIT:-50};
 
 LINTER=$(which prettier);
-LINTER=${LINTER:+"${LINTER} --write"}
+LINTER=${LINTER:+"${LINTER} --write"};
+
+FILEEXT=".map"; # removes anyting after $EXT in the URL
 
 # check if a url has been supplied
 if [ -z "$1" ]; then
@@ -11,15 +13,14 @@ if [ -z "$1" ]; then
   exit 1;
 fi
 
-function grab () {
-  let FILE = ${1};
-
-  cat ${FILE};
-}
-
 # store url with sourcemap filename
 URL="$1"
 OUTPUTDIR="$2"
+
+if [[ ! -z ${OUTPUTDIR} ]]; then
+  OUTPUTDIR="$(realpath ${OUTPUTDIR})";
+  mkdir -p ${OUTPUTDIR};
+fi
 
 # store url without sourcemap filename
 URLNOMAP=$(echo $URL | rev | cut -d '/' -f2- | rev)
@@ -27,8 +28,16 @@ URLNOMAP=$(echo $URL | rev | cut -d '/' -f2- | rev)
 # check if url is sourcemap or js
 if [ $(echo $URL | rev | awk -F'.' '{print $1}' | rev) != 'map' ]; then
   # If not .map check for a sourcemap reference
-  MAPFILE=$(curl -s $URL | grep sourceMapping | sed -e 's/.*sourceMappingURL=\([[:alnum:][:punct:]]*\)/\1/')
-  if [ -z $MAPFILE ]; then
+  if [[ -z ${OUTPUTDIR+x} ]]; then
+     _MAPFILE=$(mktemp);
+  else
+    _MAPFILE="${OUTPUTDIR}/index.js";
+  fi
+
+  curl -s "${URL}" > ${_MAPFILE};
+
+  MAPFILE=$(cat ${_MAPFILE} | grep sourceMapping | sed -e 's/.*sourceMappingURL=\([[:alnum:][:punct:]]*\)/\1/' | sed "s/\\${FILEEXT}.*/${FILEEXT}/" | awk '{ print $1 }')
+  if [ -z ${MAPFILE+x} ]; then
     echo "No sourcemap referenced in $URL"
     exit 1
   fi
@@ -37,19 +46,16 @@ if [ $(echo $URL | rev | awk -F'.' '{print $1}' | rev) != 'map' ]; then
 fi
 
 # check if file exists at all
-RESP=$(curl -s -o /dev/null -w "%{http_code}" $URL)
+RESP=$(curl -s -o /dev/null -w "%{http_code}" -- $URL)
 if [ "$RESP" != '200' ]; then
-  echo "Map not found. (HTTP Response Code: $RESP)";
+  echo "Map not found. ($URL) (HTTP Response Code: $RESP)";
   exit 1;
 fi
 
 # pull contents of the file into $MAP
-if [[ -z ${OUTPUTDIR+z} ]]; then
+if [[ -z ${OUTPUTDIR+x} ]]; then
   TMPFILE=$(mktemp);
 else
-  OUTPUTDIR="$(realpath ${OUTPUTDIR})";
-  mkdir -p ${OUTPUTDIR};
-
   TMPFILE="${OUTPUTDIR}/index.js.map";
 fi
 
@@ -73,8 +79,6 @@ if [ "$VER" != '3' ]; then
     echo "the requested sourcemap returned a version of: $VER. Trying anyway."
 fi
 
-echo "$VERSION";
-
 echo "Map loaded: read $(MAP | wc -c) bytes from $(echo $URL | rev | awk -F'/' '{print $1}' | rev)."
 
 # get the number of files, the directory structure, and the file contents
@@ -86,6 +90,7 @@ echo "$LENGTH files to be written."
 COUNTER=$LENGTH
 
 BASE=${OUTPUTDIR:-'./sourcemaps'};
+set -o noglob;
 for ((i=0;i<=LENGTH;i++)); do
   function loop () {
     # for each file: get the path without the filename, remove ../'s, remove quotes
@@ -94,19 +99,19 @@ for ((i=0;i<=LENGTH;i++)); do
     F=$(echo $SOURCES | jq .[$i] | rev | awk -F'/' '{print $1}' | rev | sed 's/\"//g');
 
     # check for source in sourcesContent, otherwise get directly from the URL.
-    DATA=$(echo $CONTENTS | jq .[$i]);
+    DATA=$(echo ${CONTENTS}| jq .[$i]);
     if [ "$DATA" == 'null' ]; then
       DATA=$(curl -s "$URLNOMAP/$P/$F");
     fi;
 
-    DATA=$(echo $DATA | sed 's/\\"/"/g');
+    DATA=$(echo ${DATA} | sed 's/\\"/"/g');
 
     # create directories to match the paths in the map, eliminate ../'s
     P=$(echo $P | sed 's/\.\.\///g');
     mkdir -p "$BASE/$P";
 
     # create the file at that location
-    echo -ne "$(echo $DATA | sed -e 's/%/%%/g' -e 's/^"//' -e 's/"$//' )\n" > "$BASE/$P/$F";
+    printf "%b\n" "$(printf '%s' "${DATA}" | sed -e 's/%/%%/g' -e 's/^"//' -e 's/"$//')" > "$BASE/$P/$F";
     echo -ne "\rWriting: $BASE/$P/$F\n";
   }
 
